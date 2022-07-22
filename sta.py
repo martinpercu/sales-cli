68b648ba20663af8b47810173270ae8f78c240de
Martin Mendez <martinpercu@gmail.com>
2022-07-22 17:05:26 +0200

    OK go

--- sta.py
+++ sta.py
@@ -1,11 +1,15 @@
 import click
 
 from clients import commands as clients_commands
+CLIENT_TABLE = '.clients.csv'
+
+
 
 @click.group()
 @click.pass_context
 def cli(ctx):
     ctx.obj = {}
+    ctx.obj['clients_table'] = CLIENT_TABLE
 
 
 cli.add_command(clients_commands.all)
